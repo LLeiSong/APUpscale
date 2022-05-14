0511a58f4be74462b16f546757d5dcf982740baa
Lei Song <lsong@clarku.edu>
2022-05-14 14:04:48 -0400

    Modify windows mcapply

--- R/mclapply_hack.R
+++ R/mclapply_hack.R
@@ -3,11 +3,16 @@
 ## Nathan VanHoudnos
 ## nathanvan AT northwestern FULL STOP edu
 ## July 14, 2014
+## Modified by Nathan VanHoudnos
+## https://github.com/nathanvan/parallelsugar/
+## blob/master/R/mclapply_socket.R
 ###########################################
 
 #' @title Windows version of parallel apply function to use
 #' @description A script to implement a hackish version of parallel:mclapply()
 #' on Windows machines.
+#' @param X (vector (atomic or list)) the vector to loop on.
+#' @param FUN the function to be applied to each element of `X` in parallel to.
 #' @param ... Any inputs works for `makeCluster`.
 #' @importFrom parallel makeCluster clusterExport parLapply stopCluster
 #' @importFrom utils sessionInfo
@@ -22,53 +27,64 @@
 
 
 ## Define the hack
-mclapply_hack <- function(...) {
+mclapply_hack <- function(X, FUN, ...) {
   ## Create a cluster
-  size.of.list <- length(list(...)[[1]])
-  cl <- makeCluster(min(size.of.list, detectCores()))
+  if (!"mc.cores" %in% list(...) | is.null(mc.cores)) {
+    mc.cores <- min(length(X), detectCores())
+  }
+  cl <- makeCluster(mc.cores)
 
-  ## Find out the names of the loaded packages
-  loaded.package.names <- c(
-    ## Base packages
-    sessionInfo()$basePkgs,
-    ## Additional packages
-    names(sessionInfo()$otherPkgs)
-  )
+  tryCatch({
+    ## Find out the names of the loaded packages
+    loaded.package.names <- c(
+      ## Base packages
+      sessionInfo()$basePkgs,
+      ## Additional packages
+      names( sessionInfo()$otherPkgs))
 
-  tryCatch(
-    {
+    ### Ship it to the clusters
+    clusterExport(cl, 'loaded.package.names',
+                  envir = environment())
 
-      ## Copy over all of the objects within scope to
-      ## all clusters.
-      this.env <- environment()
-      while (identical(this.env, globalenv()) == FALSE) {
-        clusterExport(cl,
-          ls(all.names = TRUE, envir = this.env),
-          envir = this.env
-        )
-        this.env <- parent.env(environment())
-      }
-      clusterExport(cl,
-        ls(all.names = TRUE, envir = globalenv()),
-        envir = globalenv()
-      )
+    ## Load the libraries on all the clusters
+    ## N.B. length(cl) returns the number of clusters
+    parLapply(cl, 1:length(cl), function(xx){
+      lapply(loaded.package.names, function(yy) {
+        require(yy, character.only = TRUE)})
+    })
 
-      ## Load the libraries on all the clusters
-      ## N.B. length(cl) returns the number of clusters
-      parLapply(cl, 1:length(cl), function(xx) {
-        lapply(loaded.package.names, function(yy) {
-          require(yy, character.only = TRUE)
-        })
-      })
 
-      ## Run the lapply in parallel
-      return(parLapply(cl, ...))
-    },
-    finally = {
-      ## Stop the cluster
-      stopCluster(cl)
+    .clusterExport_function(cl, FUN)
+
+    ## Run the lapply in parallel, with a special case for the ... arguments
+    if(length(list(...)) == 0) {
+      return(parLapply( cl = cl, X = X, fun = FUN))
+    } else {
+      return(parLapply( cl = cl, X = X, fun = FUN, ...))
     }
-  )
+  }, finally = {
+    ## Stop the cluster
+    parallel::stopCluster(cl)
+  })
 }
 
-## end mclapply.hack
+
+#' @title Internal function to enclose environment
+#' @description A script called by mclapply_hack to enclose environment.
+#' @param cl (vector (atomic or list)) the vector to loop on.
+#' @param FUN the function to be applied to each element of `X` in parallel to.
+#' @importFrom parallel clusterExport
+
+.clusterExport_function <- function(cl, FUN) {
+  ## Written by Hadley Wickham, off the top of his head, when I asked him
+  ##   for help at one of his Advanced R workshops.
+  env <- environment(FUN)
+  while(!identical(env, globalenv())) {
+    env <- parent.env(env)
+    clusterExport(cl, ls(all.names = TRUE, envir = env), envir = env)
+  }
+  clusterExport(cl, ls(all.names = TRUE, envir = env), envir = env)
+  ## // End Hadley Wickham
+}
+
+## end mclapply_hack
